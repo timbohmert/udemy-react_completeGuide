4dc3425d394009c1c8fd069c5b5cafb5961955c9
Tim Bohmert <bohm0050@gmail.com>
2021-03-29 23:32:13 -0500

    Worked on create and update lifecycle hooks with component class

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -4,6 +4,11 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] copnstructor');
+  }
+
   state = {
     persons: [
       { id: 'rtd456f7', name: 'Tim', age: 31 },
@@ -13,6 +18,15 @@ class App extends Component {
     otherState: 'some  other value',
     showPersons: false
   };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('App.js] getDerivedStateFromProps', props)
+    return state;
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount')
+  }
 
   nameChangeHandlder = (event, id) => {
     const personIndex = this.state.persons.findIndex((p) => {
@@ -41,6 +55,7 @@ class App extends Component {
   };
 
   render() {
+    console.log('[App.js] render')
     let persons = null;
 
     if (this.state.showPersons) {
@@ -56,6 +71,7 @@ class App extends Component {
     return (
       <div className={classes.App}>
         <Cockpit
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonsHandler}
