5eeb87313b4271fdcdfd62c50f39ab9af9168647
timbohmert <bohm0050@gmail.com>
2021-11-03 22:58:57 -0500

    Integrated React.memo() and useCallback() for components that did not always have changes with state changes.

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,22 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Button from './components/UI/Button/Button';
 
 import './App.css';
+import DemoOutput from './components/UI/Button/Demo/DemoOutput';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
 
   console.log('APP RUNNING');
 
-  const toggleParagraphHandler = () => {
+  const toggleParagraphHandler = useCallback(() => {
     setShowParagraph(
       (prevShowParagraph) => !prevShowParagraph
     );
-  };
+  }, []);
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={false} />
       <Button onClick={toggleParagraphHandler}>
         Toggle Paragraph
       </Button>
---
+++ 01-starting-setup/src/components/UI/Button/Demo/DemoOutput.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DemoOutput = (props) => {
+  console.log('DemoOuput Running');
+
+  return <p>{props.show ? 'This is new' : ''}</p>;
+};
+
+export default React.memo(DemoOutput);
