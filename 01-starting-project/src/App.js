14251352ac76e7425ca4869f45e5c727f9457e4a
timbohmert <bohm0050@gmail.com>
2021-11-14 14:22:53 -0600

    Added async and await to the fetchMoviesHandler in 01-starting-project about connecting React app with api. Also created message when no movies have loaded and when the program is fetching data.

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -6,24 +6,28 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
 
-  const fetchMoviesHandler = () => {
-    fetch('https://swapi.dev/api/films')
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map(
-          (movieData) => {
-            return {
-              id: movieData.episode_id,
-              title: movieData.title,
-              openingText: movieData.opening_crawl,
-              releaseDate: movieData.release_date
-            };
-          }
-        );
-        setMovies(data.results);
-      });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMoviesHandler = async () => {
+    setIsLoading(true);
+
+    const response = await fetch(
+      'https://swapi.dev/api/films'
+    );
+    const data = await response.json();
+
+    const transformedMovies = data.results.map(
+      (movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        };
+      }
+    );
+    setMovies(transformedMovies);
+    setIsLoading(false);
   };
 
   return (
@@ -34,7 +38,13 @@ function App() {
         </button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && (
+          <MoviesList movies={movies} />
+        )}
+        {!isLoading && movies.length === 0 && (
+          <p>Found no movies</p>
+        )}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
