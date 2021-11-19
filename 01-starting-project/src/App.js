e5d41230d37f3e4425165de6f86068eb61c3918f
timbohmert <bohm0050@gmail.com>
2021-11-18 20:06:43 -0600

    Added useEffect with the fetchMoviesHandler as dependency to run the function upon loading and not more often, and then wrapped the fetchMoviesHandler with the useCallback function to allow the function to be used as a dependency with useEffect.

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,4 +1,8 @@
-import React, { useState } from 'react';
+import React, {
+  useState,
+  useEffect,
+  useCallback
+} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -10,14 +14,14 @@ function App() {
 
   const [err, setErr] = useState(null);
 
-  const fetchMoviesHandler = async () => {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
 
     setErr(null);
 
     try {
       const response = await fetch(
-        'https://swapi.dev/api/films'
+        'https://swapi.dev/api/films/'
       );
       if (!response.ok) {
         throw new Error('Something went wrong here!');
@@ -39,11 +43,15 @@ function App() {
       setErr(error.message);
     }
     setIsLoading(false);
-  };
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, []);
 
   let content = <p>Found no movies</p>;
 
-  if (movies) {
+  if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
   }
   if (err) {
