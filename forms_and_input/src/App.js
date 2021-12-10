88f3d48f677edda5e69b0080214c33c9314c2faf
timbohmert <bohm0050@gmail.com>
2021-12-09 23:58:24 -0600

    Created project setup for forms and input section of the udemy course

---
+++ forms_and_input/src/App.js
@@ -0,0 +1,20 @@
+//section notes:
+//forms and inputs can assume different states
+//forms can be invalid or valid states for the input and lead to invalid or valid form state
+//may want to limit peoples actions if invalid
+//need to know when to validate: when form is submitted, when input is losing focus, or on every keystroke
+//when submitted: allows user to enter a valid value before getting warning, but maybe present feedback 'too late'
+//losing focus: allow users to enter valid value before warning, useful for untouched forms
+//keystroke: warns user before had a chance of entering valid values, if applied on invalid inputs it can provide more direct feedback
+
+import SimpleInput from './components/SimpleInput';
+
+function App() {
+  return (
+    <div className="app">
+      <SimpleInput />
+    </div>
+  );
+}
+
+export default App;
